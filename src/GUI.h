fbf80f8bc1efa8b8c369869e17cce4d4e95bcd8e
Konstantin Karas <konstantin.karas@tum.de>
2021-01-11 17:03:28 +0100

    added handle selection: colors still buggy when switching from arap to handle selection -> green faces still blue

--- src/GUI.h
+++ src/GUI.h
@@ -15,34 +15,43 @@ class GUI {
 public:
 
 	GUI(std::string filenameMesh) {
-		display(filenameMesh);
+		displayMesh(filenameMesh);
 	}
 
-	std::set<int> selectedFaces;
+	std::set<int> staticFaces;
+	std::set<int> faceHandles;
+
+private:
+
+	bool arapMode = false;
+	bool handleSelectionMode = false;
 
 	//https://github.com/libigl/libigl/blob/master/tutorial/708_Picking/main.cpp
-	void display(std::string filenameMesh) {
+	void displayMesh(std::string filenameMesh) {
 		//load mesh
 
 		Eigen::MatrixXd vertices, colors;
 		Eigen::MatrixXi faces;
 
-		auto& selectedFacesRef = selectedFaces;
+		auto& staticFacesRef = staticFaces;
+		auto& faceHandlesRef = faceHandles;
 
 		igl::readOFF(filenameMesh, vertices, faces);
 
 		//init white
 		colors = Eigen::MatrixXd::Constant(faces.rows(), 3, 1);
 
-		bool arapMode = false;
+		bool arapModeRef = arapMode;
+		bool handleSelectionModeRef = handleSelectionMode;
 
 		std::cout << "(Usage: [click]  Pick face on object)" << std::endl;
-		std::cout << "(Usage: [press 1]  Toggle mode)" << std::endl;
+		std::cout << "(Usage: [press 1]  Toggle between ARAP and Selection)" << std::endl;
+		std::cout << "(Usage: [press 2]  Select Handles)" << std::endl;
 		std::cout << "ARAP active" << std::endl;
 
 		igl::opengl::glfw::Viewer viewer;
 
-		viewer.callback_mouse_down = [this, &arapMode, &vertices, &faces, &colors, &selectedFacesRef] (igl::opengl::glfw::Viewer& viewer, int, int) -> bool
+		viewer.callback_mouse_down = [this, &vertices, &faces, &colors](igl::opengl::glfw::Viewer& viewer, int, int) -> bool
 		{
 			int fid;
 			Eigen::Vector3f bc;
@@ -53,42 +62,46 @@ public:
 				if (igl::unproject_onto_mesh(Eigen::Vector2f(x, y), viewer.core().view,
 					viewer.core().proj, viewer.core().viewport, vertices, faces, fid, bc))
 				{
-					if (selectedFacesRef.find(fid) == selectedFacesRef.end()) {
-						//select face
-						selectedFacesRef.insert(fid);
-						UpdateColor(fid, Eigen::Vector3d(1,0,0), colors, viewer);
-						return true;
-					}
-					else {
-						//unselect face
-						selectedFacesRef.erase(fid);
-						UpdateColor(fid, Eigen::Vector3d(1, 1, 1), colors, viewer);
-						return true;
-					}
+					ClickerHandler(staticFaces, faceHandles, fid, colors, viewer);
+					return true;
 				}
+				return false;
 			}
+			//this for free rotation during selection
 			return false;
 		};
-		viewer.callback_key_down = [this, &arapMode, &colors, &selectedFacesRef](igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier) -> bool
+
+
+		viewer.callback_key_down = [this, &colors](igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier) -> bool
 		{
+			//Toggle ARAP
 			if (key == '1') {
+				handleSelectionMode = false;
 				arapMode = !arapMode;
 				if (arapMode) {
-					for each (int fid in selectedFacesRef)
+					for each (int fid in staticFaces)
 					{
 						UpdateColor(fid, Eigen::Vector3d(0, 0, 1), colors, viewer);
 					}
 				}
 				else {
-					for each (int fid in selectedFacesRef)
+					for each (int fid in staticFaces)
 					{
+						std::cout << "blue" << std::endl;
 						UpdateColor(fid, Eigen::Vector3d(1, 0, 0), colors, viewer);
 					}
 				}
 
-				std::string out = arapMode ? "ARAP active" : "Selection active";
+				std::string out = arapMode ? "ARAP active" : "Static Face Selection active";
 				std::cout << out << std::endl;
 				return true;
+			}
+			//Toggle handle selection
+			if (key == '2') {
+				arapMode = false;
+				handleSelectionMode = !handleSelectionMode;
+				std::string out = handleSelectionMode ? "Handle Selection active" : "Static Face Selection active";
+				std::cout << out << std::endl;
 			}
 			return true;
 		};
@@ -100,10 +113,52 @@ public:
 		viewer.launch();
 	}
 
+	bool ClickerHandler(std::set<int>& staticFaces, std::set<int>& handles, int fid, Eigen::MatrixXd& colors, igl::opengl::glfw::Viewer& viewer)
+	{
+		auto& faces = handleSelectionMode ? handles : staticFaces;
+		auto newColor = handleSelectionMode ? Vector3d(0, 1, 0) : Vector3d(1, 0, 0);
 
-private:
+		if (faces.find(fid) == faces.end()) {
+			/*
+			//no static face should be a handle -> cannot be assigned
+			if (handleSelectionMode && staticFaces.find(fid) != staticFaces.end()) {
+				return false;
+			}
+			*/
+			//select face
+			faces.insert(fid);
+			UpdateColor(fid, newColor, colors, viewer);
+			std::cout << fid << std::endl;
+		}
+		else {
+			//unselect face
+			faces.erase(fid);
+			UpdateColor(fid, Eigen::Vector3d(1, 1, 1), colors, viewer);
+		}
+
+		//remove from other set if necessary, a handle cannot be static and a static face cannot be a handle
+		auto& otherFaces = handleSelectionMode ? staticFaces : handles;
+		if (otherFaces.find(fid) == otherFaces.end()) {
+			otherFaces.erase(fid);
+		}
+		return true;
+	}
+
 	void UpdateColor(int faceID, Eigen::Vector3d newColor, Eigen::MatrixXd& colors, igl::opengl::glfw::Viewer& viewer) {
 		colors.row(faceID) << newColor(0), newColor(1), newColor(2);
 		viewer.data().set_colors(colors);
 	}
+};
+
+
+//TODO make a nice menu with dropdown or buttons for modes, possibly brush size etc
+class Menu {
+public:
+	Menu(igl::opengl::glfw::Viewer viewer)
+	{
+		this->viewer = viewer;
+	}
+private:
+	igl::opengl::glfw::Viewer viewer;
+	
 };
